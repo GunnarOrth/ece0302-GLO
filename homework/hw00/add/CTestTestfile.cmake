a824ac373031c31b8770c67b19f58705c07c4d90
Gunnar Orth <glo7@pitt.edu>
2021-01-31 18:19:26 -0500

    random files generated

---
+++ homework/hw00/add/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gunnar/starter-code-ece0302/homework/hw00/add
+# Build directory: /home/gunnar/starter-code-ece0302/homework/hw00/add
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "add-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/gunnar/starter-code-ece0302/homework/hw00/add/CMakeLists.txt;16;add_test;/home/gunnar/starter-code-ece0302/homework/hw00/add/CMakeLists.txt;0;")
