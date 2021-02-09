b52be74be335cbf8fe19de0452b8a66d1fca8e4d
GunnarOrth <77905969+GunnarOrth@users.noreply.github.com>
2021-02-08 20:46:17 -0500

    Add files via upload

--- projects/project1/bitset.hpp
+++ projects/project1/bitset.hpp
@@ -1,48 +1,49 @@
 #ifndef BITSET_HPP
 #define BITSET_HPP
+#include <string>
 
 class Bitset{
 public:
 
-  // TODO COMMENT
+  //initialize n to 8 and use a for command to initialize bitst to 0
   Bitset();
 
-  // TODO COMMENT
+  //same thing but set n = size
   Bitset(intmax_t size);
 
-  // TODO COMMENT
+  //step through string with substr and for every 0 and 1 replicate in array as bool
   Bitset(const std::string & value);
 
-  // TODO COMMENT
+  // delete pointer
   ~Bitset();
 
   Bitset(const Bitset & ) = delete;
   Bitset & operator=(const Bitset &) = delete;
 
-  // TODO COMMENT
+  // im not sure what this is for
   intmax_t size() const;
 
-  // TODO COMMENT
+  // if every value in pointer is a 0 or 1 return true.
   bool good() const;
 
-  // TODO COMMENT
+  // use if statement to see if index is in parameters
   void set(intmax_t index);
 
-  // TODO COMMENT
+  // same thing but changing pointer index to 0
   void reset(intmax_t index);
 
-  // TODO COMMENT
+  // same thing but if the pointer value is 0 or 1 switch it
   void toggle(intmax_t index);
 
-  // TODO COMMENT
+  // if index is in the range and the bit at that index is 1 return true
   bool test(intmax_t index);
 
-  // TODO COMMENT
+  // use for loop to read each bit and add 0 or 1 to string as needed
   std::string asString() const;
 
 private:
-
-  // TODO
+  int n;
+  bool* array;
 };
 
 #endif
