48f439846749328ab77d8cb0a996e7a2d450346f
Gunnar Orth <glo7@pitt.edu>
2021-02-21 21:14:47 -0500

    zip

---
+++ homework/hw04/linked_list/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gunnar/starter-code-ece0302/homework/hw04/linked_list
+# Build directory: /home/gunnar/starter-code-ece0302/homework/hw04/linked_list
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_linked_list "test_linked_list")
+set_tests_properties(test_linked_list PROPERTIES  _BACKTRACE_TRIPLES "/home/gunnar/starter-code-ece0302/homework/hw04/linked_list/CMakeLists.txt;15;add_test;/home/gunnar/starter-code-ece0302/homework/hw04/linked_list/CMakeLists.txt;0;")
