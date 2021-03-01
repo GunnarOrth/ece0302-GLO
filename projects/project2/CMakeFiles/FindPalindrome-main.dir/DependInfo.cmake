ccb4f12c6bf7fd0b0919d47cb7811c8c576809f8
Gunnar Orth <glo7@pitt.edu>
2021-02-28 21:12:38 -0500

    submission

---
+++ projects/project2/CMakeFiles/FindPalindrome-main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gunnar/starter-code-ece0302/projects/project2/FindPalindrome.cpp" "/home/gunnar/starter-code-ece0302/projects/project2/CMakeFiles/FindPalindrome-main.dir/FindPalindrome.cpp.o"
+  "/home/gunnar/starter-code-ece0302/projects/project2/FindPalindrome_main.cpp" "/home/gunnar/starter-code-ece0302/projects/project2/CMakeFiles/FindPalindrome-main.dir/FindPalindrome_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ projects/project2/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gunnar/starter-code-ece0302/projects/project2
+# Build directory: /home/gunnar/starter-code-ece0302/projects/project2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "FindPalindrome-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/gunnar/starter-code-ece0302/projects/project2/CMakeLists.txt;18;add_test;/home/gunnar/starter-code-ece0302/projects/project2/CMakeLists.txt;0;")
