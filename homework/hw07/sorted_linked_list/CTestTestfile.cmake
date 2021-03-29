1695b08666999126e3801802d47fa470220d12fe
Gunnar Orth <glo7@pitt.edu>
2021-03-28 22:56:40 -0400

    sub

---
+++ homework/hw07/sorted_linked_list/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gunnar/starter-code-ece0302/homework/hw07/sorted_linked_list
+# Build directory: /home/gunnar/starter-code-ece0302/homework/hw07/sorted_linked_list
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_sorted_linked_list "test_sorted_linked_list")
+set_tests_properties(test_sorted_linked_list PROPERTIES  _BACKTRACE_TRIPLES "/home/gunnar/starter-code-ece0302/homework/hw07/sorted_linked_list/CMakeLists.txt;14;add_test;/home/gunnar/starter-code-ece0302/homework/hw07/sorted_linked_list/CMakeLists.txt;0;")
